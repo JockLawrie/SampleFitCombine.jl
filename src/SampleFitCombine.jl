e5be43fc10f010b1ef29425263b7becd952f7e02
Jock Lawrie <jock.lawrie@gmail.com>
2019-11-21 10:35:33 +1100

    House keeping.

--- src/SampleFitCombine.jl
+++ src/SampleFitCombine.jl
@@ -1,46 +1,191 @@
 module SampleFitCombine
 
 export Ensemble,
-       ProbabilityWeights,
-       sample!, fit!, combine!, predict
+       fitcomponent!,
+       combine_uniform_weights!, combine_prespecified_weights!, combine_optimal_weights!,
+       predict, loss,
+       components, ncomponents, max_ncomponents, weights  # Accessor functions
 
 using Distributions  # MixtureModel
+using Logging
 using MLJ
-using MLJBase    # target_scitype
-using Random     # GLOBAL_RNG
-using StatsBase  # sample!
+using MLJBase
+using Optim
 
-#import StatsBase.sample!  # Re-exported
-#import MLJ.fit!           # Re-exported
-import MLJ.predict         # To extend with SampleFitCombine.predict
+# Methods that this package extends
+import Distributions.components
+import Distributions.ncomponents
+import MLJ.predict
 
+################################################################################
+# Type
 
-struct Ensemble{T <: Supervised}
-    components::Vector{Machine{T}}
-    weights::Vector{Float64}
+"""
+Ensemble{M<:Model}
+  model::M
+  components::Vector{Machine{M}}  # Trained components
+  weights::Vector{Float64}        # Mixing weights
+  Kmax::Int                       # Maximum number of components
+"""
+struct Ensemble{M<:Model}
+    model::M
+    components::Vector{Machine{M}}  # Trained components
+    weights::Vector{Float64}        # Mixing weights
+    Kmax::Int                       # Maximum number of components
+
+    function Ensemble(model, components, weights, Kmax)
+        @assert Kmax > 0
+        new{typeof(model)}(model, components, weights, Kmax)
+    end
 end
 
-Ensemble(T) = Ensemble(T[], Float64[])
+Ensemble(model, Kmax) = Ensemble(model, Machine{typeof(model)}[], Float64[], Kmax)
 
+################################################################################
+# Accessor functions
 
-combine!(x) = nothing
+"Returns the vector of trained ensemble components."
+components(ensemble::Ensemble) = ensemble.components
 
+"Returns the number of trained components."
+ncomponents(ensemble::Ensemble) = length(ensemble.components)
 
-predict(ensemble::Ensemble{<:Probabilistic}, Xnew) = MixtureModel([predict(mchn, Xnew) for mchn in ensemble.components], copy(ensemble.weights))
+"Returns the maxmimum number of components allowed."
+max_ncomponents(ensemble::Ensemble) = ensemble.Kmax
 
-function predict(ensemble::Ensemble, Xnew)
-    MLJBase.target_scitype(Type{T}) == AbstractVector{<:Finite} && return predict_classifier(ensemble, Xnew)
-    result     = 0.0
-    components = ensemble.components
-    weights    = ensemble.weights
-    for (k, component) in enumerate(ensemble.components)
-        result += weights[k] * predict(component, Xnew)
+"Returns the ensemble's mixing weights."
+weights(ensemble::Ensemble) = ensemble.weights
+
+################################################################################
+# Fit
+
+function fitcomponent!(ensemble::Ensemble, X, y; rows=collect(1:length(y)))
+    ncomponents(ensemble) >= max_ncomponents(ensemble) && error("Ensemble already has K=$(max_ncomponents(ensemble)) trained components")
+    mchn = machine(ensemble.model, X, y)
+    fit!(mchn, rows=rows)
+    push!(components(ensemble), mchn)
+end
+
+################################################################################
+# Combine
+
+function combine_uniform_weights!(ensemble)
+    empty!(weights(ensemble))
+    nk = ncomponents(ensemble)
+    w  = 1.0 / nk
+    for k = 1:nk
+        push!(weights(ensemble), w)
+    end
+    nothing
+end
+
+function combine_prespecified_weights!(ensemble, w)
+    # Checks
+    nk  = ncomponents(ensemble)
+    nk == 0 && @warn "Ensemble has no components to combine." && return
+    nw  = length(w)
+    nw != nk && error("$(nw) weights were supplied to combine!, expecting $(nk) weights, 1 for each component.")
+
+    # Ensure that ensemble.weights has nk elements
+    wt = weights(ensemble)
+    nw = length(wt)
+    if nw < nk
+        for k = (nw + 1):nk
+            push!(wt, 0.0)
+        end
+    elseif nw > nk
+        for k = 1:(nw - nk)
+            pop!(wt)
+        end
+    end
+
+    # Replace existing weights with new weights
+    for k = 1:nk
+        @inbounds wt[k] = w[k]
+    end
+    nothing
+end
+
+function combine_optimal_weights!(ensemble, X, y, lossfunc)
+    logits = fill(0.0, ncomponents(ensemble) - 1)
+    w      = fill(0.0, ncomponents(ensemble))
+    pred_components = construct_prediction_components(ensemble, X[1])
+    function f(b)
+        computeweights!(w, b)
+        combine_prespecified_weights!(ensemble, w)
+        loss!(pred_components, ensemble, X, y, lossfunc)
+    end
+    theta0 = logits
+    opts   = Optim.Options(time_limit=60, f_tol=1e-12)   # Debug with show_trace=true
+    mdl    = optimize(f, theta0, LBFGS(), opts;) # TODO: optimize(objective, theta0, LBFGS(), opts; autodiff=:forward)
+    theta1 = mdl.minimizer
+    computeweights!(w, theta1)
+    combine_prespecified_weights!(ensemble, w)
+    nothing
+end
+
+function computeweights!(result, logits)
+    wtotal    = 1.0
+    result[1] = 1.0  # exp(0.0)
+    K = length(result)
+    for k = 2:K
+        @inbounds w = exp(logits[k-1])
+        @inbounds result[k] = w
+        wtotal += w
+    end
+    result ./= wtotal
+end
+
+################################################################################
+# Predict
+
+function predict(ensemble::Ensemble, Xtest::Vector{Vector{Float64}})
+    n = size(Xtest, 1)
+    pred_components = construct_prediction_components(ensemble, Xtest[1])
+    pred1     = predict!(ensemble, Xtest[1], pred_components)
+    result    = Vector{typeof(pred1)}(undef, n)
+    result[1] = pred1
+    for i = 2:n
+        result[i] = predict!(ensemble, Xtest[i], pred_components)
     end
     result
 end
 
-#losses = [loss(yhati, yi) for (yhati, yi) in zip(yhat, y)]
-#w = ProbabilityWeights(pw; wsum=sum(losses))
+predict(ensemble::Ensemble, Xtest::Vector{Float64}) = predict(ensemble, [Xtest])
 
+function predict!(ensemble::Ensemble, Xrow::Vector{Float64}, pred_components)
+    for (k, mchn) in enumerate(components(ensemble))
+        pred_components[k] = predict(mchn, Xrow)
+    end
+    MixtureModel(pred_components, weights(ensemble))
+end
+
+predict(mchn::Machine, Xnew::Vector{Float64})         =  MLJBase.predict(mchn.model, mchn.fitresult, Xnew)
+predict(mchn::Machine, Xnew::Vector{Vector{Float64}}) = [MLJBase.predict(mchn.model, mchn.fitresult, Xnew) for x in Xnew]
+
+function construct_prediction_components(ensemble, Xtest::Vector{Float64})
+    T = typeof(predict(components(ensemble)[1], Xtest))
+    Vector{T}(undef, ncomponents(ensemble))
+end
+
+################################################################################
+# Loss
+
+"Returns: lossfunc(predict(ensemble, X), y)"
+function loss(ensemble, X::Vector{Vector{Float64}}, y::Vector{Float64}, lossfunc)
+    size(X, 1) != length(y) && error("X and y do not have the same number of observations.")
+    pred_components = construct_prediction_components(ensemble, X[1])
+    loss!(pred_components, ensemble, X, y, lossfunc)
+end
+
+function loss!(pred_components, ensemble, X, y, lossfunc)
+    result = 0.0
+    n = length(y)
+    for i = 1:n
+        yhat    = predict!(ensemble, X[i], pred_components)
+        result += lossfunc(yhat, y[i])
+    end
+    result / n
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,191 @@
-using SampleFitCombine
 using Test
+using SampleFitCombine
 
-#include("bagging.jl")
-include("sequential_weighting.jl")
+using Clustering
+using Distributions  # Normal(m, s)
+using Logging
+using MLJ
+using MLJBase
+using StatsBase  # wsample!, ProbabilityWeights
+using UnicodePlots
+
+import SampleFitCombine.weights  # Because StatsBase also exports weights, so we need to specify which method we want
+
+################################################################################
+# Model
+
+"Normal density estimate with no predictor variables."
+mutable struct NormalDensity <: MLJBase.Probabilistic
+end
+
+MLJBase.target_scitype(::Type{NormalDensity}) = AbstractVector{<:MLJ.Continuous}
+
+function MLJBase.fit(model::NormalDensity, verbosity::Integer, X, y)
+    n = length(y)
+    W = 0.0  # running total weight
+    A = 0.0  # running mean
+    Q = 0.0  # running sum of squares (the numerator of the variance)
+    for i = 1:n
+        w  = 1.0  # TODO: Allow other weights to be passed in
+        w == 0.0 && continue
+        yi = y[i]
+        W += w
+        Aprev = A
+        A += w * (yi - A) / W
+        Q += w * (yi - Aprev) * (yi - A)
+    end
+    [A, sqrt(Q/W)], nothing, nothing  # fitresult, cache, report
+end
+
+MLJBase.predict(model::NormalDensity, fitresult, Xnew) = Normal(fitresult[1], fitresult[2])
+
+################################################################################
+# Sample-Fit-Combine: Cluster components
+
+"""
+1. Start with K = 1 cluster and all observations assigned to the cluster.
+2. Measure loss.
+3. If another cluster is warranted (stopping criteria not satisfied):
+   - Set K = K + 1
+   - Identify K clusters in probs, and assign each observation to a cluster.
+   - Remove all existing components and weights
+   - For k = 1:K
+         Set Xtrain, ytrain the observations in cluster k
+         Estimate a new component on Xtrain, ytrain
+"""
+function clustercomponents!(ensemble, X, y, lossfunc)
+    # Fit 1 cluster
+    fitcomponent!(ensemble, X, y)
+    combine_uniform_weights!(ensemble)     # Give the component a weight of 1
+
+    # Fit k clusters, k = 2:K
+    Lmin = loss(ensemble, X, y, lossfunc)  # Minimum loss achieved so far
+    N    = length(y)
+    Kmax = max_ncomponents(ensemble)
+    for k = 2:Kmax
+        pred  = predict(ensemble, X)
+        probs = reshape([cdf(pred[i], y[i]) for i = 1:N], 1, N)
+        res   = kmeans(probs, k)
+        a     = assignments(res)
+        old_components = copy(components(ensemble))
+        old_weights    = copy(weights(ensemble))
+        for k2 = 1:(k-1)
+            popfirst!(components(ensemble))
+        end
+        for cluster = 1:k
+            # Sample
+            sample = a .== cluster
+            ytrain = y[sample]
+            Xtrain = X[sample]
+
+            # Fit
+            fitcomponent!(ensemble, Xtrain, ytrain)
+        end
+        combine_optimal_weights!(ensemble, X, y, lossfunc)  # Combine
+
+        # Stopping criteria
+        L = loss(ensemble, X, y, lossfunc)
+        @info "K = $(k). Loss = $(L). Lmin = $(Lmin)"
+        if L > Lmin || abs((L - Lmin)/Lmin) <= 0.005
+            for k2 = 1:k
+                popfirst!(components(ensemble))
+            end
+            for k2 = 1:length(old_components)
+                push!(components(ensemble), old_components[k2])
+            end
+            combine_prespecified_weights!(ensemble, old_weights)
+            break
+        end
+        Lmin = L
+        ncomponents(ensemble) == max_ncomponents(ensemble) && break
+    end
+    Lmin
+end
+
+################################################################################
+println("")
+@info "Example 1: Data: 2 Normal components. Sample: simple random sample. combine: uniform_weights."
+@info "    Also known as Bagging"
+
+# Data: Y ~ 0.5*N(10, 2^2) + 0.5*N(20, 2^2)
+N = 1000
+X = fill([1.0], N)
+y = vcat(10.0 .+ 2.0*randn(Int(0.5*N)), 20.0 .+ 2.0*randn(Int(0.5*N)))
+#histogram(y, nbins=30)
+
+# Sample-Fit-Combine
+ensemble = Ensemble(NormalDensity(), 10)  # Maximum of components
+samplingfraction = 0.8
+n        = Int(round(samplingfraction * N))         # Sample size
+w        = ProbabilityWeights([1.0/N for i = 1:N])  # Sample weights
+i_all    = collect(1:N)  # Population row indices
+i_train  = fill(0, n)    # Sample row indices
+for k = 1:max_ncomponents(ensemble)
+    wsample!(i_all, w, i_train; replace=true, ordered=false);  # Sample
+    fitcomponent!(ensemble, X, y; rows=i_train)  # Fit
+end
+combine_uniform_weights!(ensemble)  # Combine
+for machine in components(ensemble)
+    println(predict(machine, X[1]))
+end
+println(weights(ensemble))
+
+# loss
+lossfunc = (yhat, y) -> -logpdf(yhat, y)
+loss_bagging = loss(ensemble, X, y, lossfunc)
+
+# Predict
+Xtest = X[1]
+ytest = y[1]
+println(predict(ensemble, Xtest))
+
+# Alternative combine: Combine components with pre-specified weights
+w   = rand(max_ncomponents(ensemble))
+w ./= sum(w)
+combine_prespecified_weights!(ensemble, w)
+println(weights(ensemble))
+println(loss(ensemble, X, y, lossfunc))
+
+# Alternative combine: Combine components with optimized weights
+combine_optimal_weights!(ensemble, X, y, lossfunc)
+println(weights(ensemble))
+println(loss(ensemble, X, y, lossfunc))
+
+################################################################################
+println("")
+@info "Example 2: Data: unchanged. Sample-Fit-Combine: clustercomponents."
+
+# Sample-Fit-Combine
+ensemble = Ensemble(NormalDensity(), 10)  # Maximum of 10 components
+lossfunc = (yhat, y) -> -logpdf(yhat, y)
+clustercomponents!(ensemble, X, y, lossfunc)
+for machine in components(ensemble)
+    println(predict(machine, X[1]))
+end
+println(weights(ensemble))
+
+# loss
+loss_cc = loss(ensemble, X, y, lossfunc)
+@info "Bagging: loss = $(loss_bagging);  Cluster components: loss = $(loss_cc)"
+
+################################################################################
+println("")
+@info "Example 3: Data: 3 components. Sample-Fit-Combine: clustercomponents."
+
+# Data: Y ~ 0.333*N(10, 2^2) + 0.333*N(17, 2^2) + 0.333*N(24, 2^2)
+N = 1500
+X = fill([1.0], N)
+y = vcat(10.0 .+ 2.0*randn(round(Int(N/3))), 17.0 .+ 2.0*randn(round(Int(N/3))), 24.0 .+ 2.0*randn(round(Int(N/3))));
+#histogram(y, nbins=50)
+
+# Sample-Fit-Combine
+ensemble = Ensemble(NormalDensity(), 10)  # Maximum of 10 components
+lossfunc = (yhat, y) -> -logpdf(yhat, y)
+clustercomponents!(ensemble, X, y, lossfunc)
+for machine in components(ensemble)
+    println(predict(machine, X[1]))
+end
+println(weights(ensemble))
+
+# loss
+println(loss(ensemble, X, y, lossfunc))
